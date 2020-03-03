134fb3d9b0dbd7411ef4cb4f67bf9c60447d52de
DevDavisNunez <devdavisnunez@gmail.com>
2020-03-02 20:45:54 -0600

    Update Langs ;)
    Added French, German.

--- AUTOPLUG2/lang/Langdatabase.lua
+++ AUTOPLUG2/lang/Langdatabase.lua
@@ -2,6 +2,8 @@ Langs = {
 { id =  "ENGLISH_US",	version = "01.00",		author =  "gdljjrod" },
 { id =  "ITALIAN",		version = "01.00",		author =  "TheHeroGAC" },
 { id =  "JAPANESE",		version = "01.00",		author =  "Kouchan" },
-{ id =  "SPANISH",		version = "01.00",		author =  "gdljjrod" },
+{ id =  "SPANISH",		version = "01.01",		author =  "gvaldebenit" },
 { id =  "CHINESE_S",	version = "01.00",		author =  "yexun1995" },
+{ id =  "FRENCH",		version = "01.00",		author =  "Chronoss09" },
+{ id =  "GERMAN",		version = "01.00",		author =  "Schn1ek3" },
 }
