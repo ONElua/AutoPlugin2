47d8efb234cf8b350557dda5353e9819bbec2452
DevDavisNunez <devdavisnunez@gmail.com>
2020-03-19 08:14:12 -0600

    Update Langs DB.
    - Italian
    - French

--- AUTOPLUG2/lang/Langdatabase.lua
+++ AUTOPLUG2/lang/Langdatabase.lua
@@ -1,10 +1,10 @@
 Langs = {
 { id =  "ENGLISH_US",	version = "01.04",		author =  "Yoti" },
-{ id =  "ITALIAN",		version = "01.05",		author =  "TheHeroGAC" },
+{ id =  "ITALIAN",		version = "01.06",		author =  "TheHeroGAC" },
 { id =  "JAPANESE",		version = "01.04",		author =  "Kouchan" },
 { id =  "SPANISH",		version = "01.05",		author =  "gvaldebenit" },
 { id =  "CHINESE_S",	version = "01.06",		author =  "yexun1995" },
-{ id =  "FRENCH",		version = "01.04",		author =  "Chronoss09" },
+{ id =  "FRENCH",		version = "01.05",		author =  "Chronoss09" },
 { id =  "GERMAN",		version = "01.04",		author =  "Schn1ek3" },
 { id =  "RUSSIAN",		version = "01.04",		author =  "Yoti" },
 { id =  "POLISH",		version = "01.03",		author =  "Gameplay World's" }
