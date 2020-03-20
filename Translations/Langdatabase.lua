49524e267e0f202c6aeee66fdb048325e7567947
DevDavisNunez <devdavisnunez@gmail.com>
2020-03-20 16:23:22 -0600

    Update Langs DB.
    Update FRENCH by @chronoss09

--- Translations/Langdatabase.lua
+++ Translations/Langdatabase.lua
@@ -4,7 +4,7 @@ Online_Langs = {
 { id =  "JAPANESE",		version = "01.04",		author =  "Kouchan" },
 { id =  "SPANISH",		version = "01.06",		author =  "gvaldebenit" },
 { id =  "CHINESE_S",	version = "01.06",		author =  "yexun1995" },
-{ id =  "FRENCH",		version = "01.06",		author =  "Chronoss09" },
+{ id =  "FRENCH",		version = "01.07",		author =  "Chronoss09" },
 { id =  "GERMAN",		version = "01.04",		author =  "Schn1ek3" },
 { id =  "RUSSIAN",		version = "01.05",		author =  "Yoti" },
 { id =  "POLISH",		version = "01.03",		author =  "Gameplay World's" }
