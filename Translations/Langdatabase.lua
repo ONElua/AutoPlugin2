e0e0ea04ff4236f09b6899eddcf9fb79b5836c54
DevDavisNunez <devdavisnunez@gmail.com>
2020-04-16 12:03:26 -0500

    Update Langs DB.
    Thanks for all your help keeping the project updated and translated in several languages. @yoti @theherogac @yexun1995  @chronoss09

--- Translations/Langdatabase.lua
+++ Translations/Langdatabase.lua
@@ -1,11 +1,11 @@
 Online_Langs = {
 { id =  "ENGLISH_US",	version = "01.09",		author =  "Yoti" },
-{ id =  "ITALIAN",		version = "01.11",		author =  "TheHeroGAC" },
+{ id =  "ITALIAN",		version = "01.12",		author =  "TheHeroGAC" },
 { id =  "JAPANESE",		version = "01.09",		author =  "Kouchan" },
 { id =  "SPANISH",		version = "01.12",		author =  "gvaldebenit" },
-{ id =  "CHINESE_S",	version = "01.14",		author =  "yexun1995" },
-{ id =  "FRENCH",		version = "01.14",		author =  "Chronoss09" },
+{ id =  "CHINESE_S",	version = "01.15",		author =  "yexun1995" },
+{ id =  "FRENCH",		version = "01.15",		author =  "Chronoss09" },
 { id =  "GERMAN",		version = "01.09",		author =  "Schn1ek3" },
-{ id =  "RUSSIAN",		version = "01.13",		author =  "Yoti" },
+{ id =  "RUSSIAN",		version = "01.14",		author =  "Yoti" },
 { id =  "POLISH",		version = "01.08",		author =  "Gameplay World's" },
 }
