2ee8554d0a91470044ce6741f430af78388990b8
ONELua Team <oneluaproject@gmail.com>
2020-05-05 20:33:42 -0500

    Online update for psp plugins

---
+++ AUTOPLUG2/plugins/plugins_psp.lua
@@ -0,0 +1,25 @@
+pluginsP = {
+
+{	name = "CXMB", name="cxmb.prx", path = "cxmb/", txt = "vsh.txt", version = "0001", desc = LANGUAGE["CXMB_DESC"], KEY = "CXMB_DESC", },
+{	name = "GTA Native Resolution Patch v0.2", name = "gta_native.prx",	path = "seplugins/", txt = "game.txt", version = "0001", desc = LANGUAGE["GTA_PATCH_DESC"], KEY = "GTA_PATCH_DESC", },
+{	name = "Camera Patch Lite", name = "camera_patch_lite.prx",	path = "seplugins/", txt = "game.txt", config = "camera_patch_lite.ini", version = "0001", desc = LANGUAGE["CAMERA_PATCH_DESC"], KEY = "CAMERA_PATCH_DESC", },
+{	name = "Lang Swapper", name = "LangSwapper.prx", path = "seplugins/", txt = "game.txt", version = "0001", desc = LANGUAGE["LANGSWAPPER_DESC"], KEY = "LANGSWAPPER_DESC", },
+{	name = "Category lite", name = "category_lite.prx",	path = "seplugins/", txt = "vsh.txt", version = "0001", desc = LANGUAGE["CATEGORYLITE_DESC"], KEY = "CATEGORYLITE_DESC", },
+{	name = "Kingdom Hearts: Birth By Sleep/Final Mix UNDUB", name = "bbsfmUndub.prx", path = "seplugins/", txt = "game.txt", version = "0001", desc = LANGUAGE["KHBBS_UNDUB_DESC"], KEY = "KHBBS_UNDUB_DESC", },
+
+}
+
+psp_plugins = {
+
+{ name = "Grand Theft Auto Remastered",		path = "gta_remastered.prx",			version = "0001", desc = LANGUAGE["GTA_REMASTERED_DESC"], KEY = "GTA_REMASTERED_DESC", },
+{ name = "Grand Theft Auto Remastered V2",	path = "gta_remastered_v2.prx",			version = "0001", desc = LANGUAGE["GTAV2_REMASTERED_DESC"], KEY = "GTAV2_REMASTERED_DESC", },
+{ name = "Kingdom Hearts Remastered",		path = "khbbs_remastered.prx",			version = "0001", desc = LANGUAGE["KHBBS_REMASTERED_DESC"], KEY = "KHBSS_REMASTERED_DESC", },
+{ name = "Metal Gear Solid Remastered",		path = "mgs_remastered.prx",			version = "0001", desc = LANGUAGE["METALGEARSOLID_REMASTERED_DESC"], KEY = "METALGEARSOLID_REMASTERED_DESC", },
+{ name = "Prince Of Persia Remastered",		path = "pop_remastered.prx",			version = "0001", desc = LANGUAGE["PRINCEOFPERSIA_REMASTERED_DESC"], KEY = "PRINCEOFPERSIA_REMASTERED_DESC", },
+{ name = "Resistance Remastered",			path = "resistance_remastered.prx",		version = "0001", desc = LANGUAGE["RESISTANCE_REMASTERED_DESC"], KEY = "RESISTANCE_REMASTERED_DESC", },
+{ name = "SplinterCell Remastered",			path = "splintercell_remastered.prx",	version = "0001", desc = LANGUAGE["SPLINTER_REMASTERED_DESC"], KEY = "SPLINTER_REMASTERED_DESC", },
+{ name = "Tomb Raider Remastered",			path = "tombraider_remastered.prx", 	version = "0001", desc = LANGUAGE["TOMBRAIDER_REMASTERED_DESC"], KEY = "TOMBRAIDER_REMASTERED_DESC", },
+{ name = "Warriors Remastered",				path = "warriors_remastered.prx", 		version = "0001", desc = LANGUAGE["WARRIORS_REMASTERED_DESC"], KEY = "WARRIORS_REMASTERED_DESC", },
+{ name = "Tony Hawk Remastered",			path = "warriors_remastered.prx", 		version = "0001", desc = LANGUAGE["TONYHAWKS_REMASTERED_DESC"], KEY = "TONYHAWKS_REMASTERED_DESC", },
+
+}
