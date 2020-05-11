6269de968d90586fa1b6c7290c9d3a6c48c9efd0
DevDavisNunez <devdavisnunez@gmail.com>
2020-05-11 17:55:44 -0500

    Update Langs DB.
    Thanks for translations!

--- AUTOPLUG2/lang/Langdatabase.lua
+++ AUTOPLUG2/lang/Langdatabase.lua
@@ -1,11 +1,11 @@
 Langs = {
 { id =  "ENGLISH_US",	version = "01.11",		author =  "Yoti" },
-{ id =  "ITALIAN",		version = "01.14",		author =  "TheHeroGAC" },
+{ id =  "ITALIAN",		version = "01.15",		author =  "TheHeroGAC" },
 { id =  "JAPANESE",		version = "01.11",		author =  "Kouchan" },
-{ id =  "SPANISH",		version = "01.15",		author =  "gvaldebenit" },
+{ id =  "SPANISH",		version = "01.16",		author =  "gvaldebenit" },
 { id =  "CHINESE_S",	version = "01.17",		author =  "yexun1995" },
 { id =  "FRENCH",		version = "01.17",		author =  "Chronoss09" },
 { id =  "GERMAN",		version = "01.11",		author =  "Schn1ek3" },
-{ id =  "RUSSIAN",		version = "01.16",		author =  "Yoti" },
+{ id =  "RUSSIAN",		version = "01.17",		author =  "Yoti" },
 { id =  "POLISH",		version = "01.11",		author =  "Gameplay World's" },
 }
