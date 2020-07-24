6f14bfb3e7d30b87d616df60b1d0d6cdeffee480
DevDavisNunez <devdavisnunez@gmail.com>
2020-07-24 15:54:42 -0500

    Update Langs DB.
    Russian thanks to Yoti! :+1:

--- Translations/Langdatabase.lua
+++ Translations/Langdatabase.lua
@@ -6,7 +6,7 @@ Online_Langs = {
 { id =  "CHINESE_S",	version = "01.26",		author =  "yexun1995" },
 { id =  "FRENCH",		version = "01.25",		author =  "Chronoss09" },
 { id =  "GERMAN",		version = "01.17",		author =  "Schn1ek3" },
-{ id =  "RUSSIAN",		version = "01.25",		author =  "Yoti" },
+{ id =  "RUSSIAN",		version = "01.26",		author =  "Yoti" },
 { id =  "POLISH",		version = "01.17",		author =  "Gameplay World's" },
 { id =  "PORTUGUESE_BR",version = "01.03",		author =  "allanaln2009" },
 }
