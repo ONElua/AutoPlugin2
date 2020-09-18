e47c4bfb9ac74fa63285d7bdb7691b9f402138d1
ONELua Team <oneluaproject@gmail.com>
2020-09-17 20:19:38 -0500

    Fix error french.lua

--- Translations/Langdatabase.lua
+++ Translations/Langdatabase.lua
@@ -4,7 +4,7 @@ Online_Langs = {
 { id =  "JAPANESE",		version = "01.18",		author =  "Kouchan" },
 { id =  "SPANISH",		version = "01.24",		author =  "gvaldebenit" },
 { id =  "CHINESE_S",	version = "01.28",		author =  "yexun1995" },
-{ id =  "FRENCH",		version = "01.28",		author =  "Chronoss09" },
+{ id =  "FRENCH",		version = "01.29",		author =  "Chronoss09" },
 { id =  "GERMAN",		version = "01.18",		author =  "Schn1ek3" },
 { id =  "RUSSIAN",		version = "01.28",		author =  "Yoti" },
 { id =  "POLISH",		version = "01.18",		author =  "Gameplay World's" },
