6db224442290e0998f272759393dbb888552c3f5
ONELua Team <oneluaproject@gmail.com>
2020-10-02 16:57:47 -0500

    Fix Update RUSSIAN

--- Translations/Langdatabase.lua
+++ Translations/Langdatabase.lua
@@ -3,10 +3,10 @@ Online_Langs = {
 { id =  "ITALIAN",		version = "01.23",		author =  "TheHeroGAC" },
 { id =  "JAPANESE",		version = "01.19",		author =  "Kouchan" },
 { id =  "SPANISH",		version = "01.25",		author =  "gvaldebenit" },
-{ id =  "CHINESE_S",	version = "01.31",		author =  "yexun1995" },
+{ id =  "CHINESE_S",	version = "01.30",		author =  "yexun1995" },
 { id =  "FRENCH",		version = "01.30",		author =  "Chronoss09" },
 { id =  "GERMAN",		version = "01.19",		author =  "Schn1ek3" },
-{ id =  "RUSSIAN",		version = "01.29",		author =  "Yoti" },
+{ id =  "RUSSIAN",		version = "01.30",		author =  "Yoti" },
 { id =  "POLISH",		version = "01.19",		author =  "Gameplay World's" },
 { id =  "PORTUGUESE_BR",version = "01.06",		author =  "allanaln2009" },
 }
