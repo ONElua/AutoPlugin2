36106057f96b8c9dcdb7187baa7701cecb30deef
ONELua Team <oneluaproject@gmail.com>
2020-10-03 12:39:16 -0500

    Update CHINESE_S.lua

--- Translations/Langdatabase.lua
+++ Translations/Langdatabase.lua
@@ -3,7 +3,7 @@ Online_Langs = {
 { id =  "ITALIAN",		version = "01.24",		author =  "TheHeroGAC" },
 { id =  "JAPANESE",		version = "01.20",		author =  "Kouchan" },
 { id =  "SPANISH",		version = "01.26",		author =  "gvaldebenit" },
-{ id =  "CHINESE_S",	version = "01.31",		author =  "yexun1995" },
+{ id =  "CHINESE_S",	version = "01.32",		author =  "yexun1995" },
 { id =  "FRENCH",		version = "01.31",		author =  "Chronoss09" },
 { id =  "GERMAN",		version = "01.20",		author =  "Schn1ek3" },
 { id =  "RUSSIAN",		version = "01.31",		author =  "Yoti" },
