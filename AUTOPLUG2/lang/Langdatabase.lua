5680e039ca21827dfa857053cd62b4279c9d4c34
ONELua Team <oneluaproject@gmail.com>
2020-10-11 13:57:30 -0500

    Update FRENCH.lua

--- AUTOPLUG2/lang/Langdatabase.lua
+++ AUTOPLUG2/lang/Langdatabase.lua
@@ -4,7 +4,7 @@ Langs = {
 { id =  "JAPANESE",		version = "01.20",		author =  "Kouchan" },
 { id =  "SPANISH",		version = "01.26",		author =  "gvaldebenit" },
 { id =  "CHINESE_S",	version = "01.32",		author =  "yexun1995" },
-{ id =  "FRENCH",		version = "01.31",		author =  "Chronoss09" },
+{ id =  "FRENCH",		version = "01.32",		author =  "Chronoss09" },
 { id =  "GERMAN",		version = "01.20",		author =  "Schn1ek3" },
 { id =  "RUSSIAN",		version = "01.32",		author =  "Yoti" },
 { id =  "POLISH",		version = "01.20",		author =  "Gameplay World's" },
