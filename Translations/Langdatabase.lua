042180cecc7cf1f9db509466b18da40931fe3789
ONELua Team <oneluaproject@gmail.com>
2020-11-03 18:55:15 -0600

    Update FRENCH.lua

--- Translations/Langdatabase.lua
+++ Translations/Langdatabase.lua
@@ -4,7 +4,7 @@ Online_Langs = {
 { id =  "JAPANESE",		version = "01.24",		author =  "Kouchan" },
 { id =  "SPANISH",		version = "01.31",		author =  "gvaldebenit" },
 { id =  "CHINESE_S",	version = "01.37",		author =  "yexun1995" },
-{ id =  "FRENCH",		version = "01.38",		author =  "Chronoss09" },
+{ id =  "FRENCH",		version = "01.39",		author =  "Chronoss09" },
 { id =  "GERMAN",		version = "01.24",		author =  "Schn1ek3" },
 { id =  "RUSSIAN",		version = "01.36",		author =  "Yoti" },
 { id =  "POLISH",		version = "01.24",		author =  "Gameplay World's" },
