dca9fdd8adfdc17e1db767d88d4dcb672239c958
ONELua Team <oneluaproject@gmail.com>
2021-02-14 12:24:52 -0600

    Update SPANISH.lua

--- AUTOPLUG2/lang/Langdatabase.lua
+++ AUTOPLUG2/lang/Langdatabase.lua
@@ -2,7 +2,7 @@ Langs = {
 { id =  "ENGLISH_US",	version = "01.28",		author =  "Yoti" },
 { id =  "ITALIAN",		version = "01.32",		author =  "TheHeroGAC" },
 { id =  "JAPANESE",		version = "01.28",		author =  "Kouchan" },
-{ id =  "SPANISH",		version = "01.37",		author =  "gvaldebenit" },
+{ id =  "SPANISH",		version = "01.38",		author =  "gvaldebenit" },
 { id =  "CHINESE_S",	version = "01.42",		author =  "yexun1995" },
 { id =  "FRENCH",		version = "01.44",		author =  "Chronoss09" },
 { id =  "GERMAN",		version = "01.28",		author =  "Schn1ek3" },
