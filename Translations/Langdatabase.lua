98b1d073cadc5b69f388b8de95927ff3df52a83c
ONELua Team <oneluaproject@gmail.com>
2021-02-23 18:36:00 -0600

    Update FRENCH & POLISH

--- Translations/Langdatabase.lua
+++ Translations/Langdatabase.lua
@@ -4,9 +4,9 @@ Online_Langs = {
 { id =  "JAPANESE",		version = "01.29",		author =  "Kouchan" },
 { id =  "SPANISH",		version = "01.39",		author =  "tetsuoScene" },
 { id =  "CHINESE_S",	version = "01.43",		author =  "yexun1995" },
-{ id =  "FRENCH",		version = "01.45",		author =  "Chronoss09" },
+{ id =  "FRENCH",		version = "01.46",		author =  "Chronoss09" },
 { id =  "GERMAN",		version = "01.29",		author =  "Schn1ek3" },
 { id =  "RUSSIAN",		version = "01.45",		author =  "Yoti" },
-{ id =  "POLISH",		version = "01.29",		author =  "Gameplay World's" },
+{ id =  "POLISH",		version = "01.30",		author =  "KierownikStacji" },
 { id =  "PORTUGUESE_BR",version = "01.16",		author =  "allanaln2009" },
 }
