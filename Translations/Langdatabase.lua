03b8530aab0a454f21eed74ad365bfbb9fadcac9
ONELua Team <oneluaproject@gmail.com>
2021-05-11 17:41:21 -0500

    RUSSIAN Updated

--- Translations/Langdatabase.lua
+++ Translations/Langdatabase.lua
@@ -6,7 +6,7 @@ Online_Langs = {
 { id =  "CHINESE_S",	version = "01.49",		author =  "yexun1995" },
 { id =  "FRENCH",		version = "01.53",		author =  "Chronoss09" },
 { id =  "GERMAN",		version = "01.35",		author =  "Schn1ek3" },
-{ id =  "RUSSIAN",		version = "01.52",		author =  "Yoti" },
+{ id =  "RUSSIAN",		version = "01.53",		author =  "Yoti" },
 { id =  "POLISH",		version = "01.36",		author =  "KierownikStacji" },
 { id =  "PORTUGUESE_BR",version = "01.22",		author =  "allanaln2009" },
 }
