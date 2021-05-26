fb2e83cd986b2d6a36dd2d267332fab66518c11b
ONELua Team <oneluaproject@gmail.com>
2021-05-25 20:59:29 -0500

    Updated Langs

--- AUTOPLUG2/lang/Langdatabase.lua
+++ AUTOPLUG2/lang/Langdatabase.lua
@@ -1,9 +1,9 @@
 Langs = {
 { id =  "ENGLISH_US",	version = "01.37",		author =  "Yoti" },
 { id =  "ITALIAN",		version = "01.41",		author =  "TheHeroGAC" },
-{ id =  "JAPANESE",		version = "01.37",		author =  "Kouchan" },
-{ id =  "SPANISH",		version = "01.47",		author =  "tetsuoScene" },
-{ id =  "CHINESE_S",	version = "01.51",		author =  "yexun1995" },
+{ id =  "JAPANESE",		version = "01.38",		author =  "Kouchan" },
+{ id =  "SPANISH",		version = "01.48",		author =  "tetsuoScene" },
+{ id =  "CHINESE_S",	version = "01.52",		author =  "yexun1995" },
 { id =  "FRENCH",		version = "01.55",		author =  "Chronoss09" },
 { id =  "GERMAN",		version = "01.37",		author =  "Schn1ek3" },
 { id =  "RUSSIAN",		version = "01.55",		author =  "Yoti" },
