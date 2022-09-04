e16eed08bd5e42bbc88c5ee462f79b796a38e102
ONELua Team <oneluaproject@gmail.com>
2022-09-04 17:59:25 -0500

    Update FRENCH

--- AUTOPLUG2/lang/Langdatabase.lua
+++ AUTOPLUG2/lang/Langdatabase.lua
@@ -4,7 +4,7 @@ Langs = {
 { id =  "JAPANESE",		version = "01.45",		author =  "Kouchan" },
 { id =  "SPANISH",		version = "01.56",		author =  "tetsuoScene" },
 { id =  "CHINESE_S",	version = "01.62",		author =  "yexun1995" },
-{ id =  "FRENCH",		version = "01.62",		author =  "Chronoss09" },
+{ id =  "FRENCH",		version = "01.63",		author =  "Chronoss09" },
 { id =  "GERMAN",		version = "01.44",		author =  "Schn1ek3" },
 { id =  "RUSSIAN",		version = "01.64",		author =  "Yoti" },
 { id =  "POLISH",		version = "01.45",		author =  "KierownikStacji" },
