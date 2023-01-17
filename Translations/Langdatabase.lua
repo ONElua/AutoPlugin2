5d429edd5b5c29fb33889e445aed4bb0f26f8c59
ONELua Team <oneluaproject@gmail.com>
2023-01-16 18:10:29 -0600

    Spanish lang

--- Translations/Langdatabase.lua
+++ Translations/Langdatabase.lua
@@ -2,7 +2,7 @@ Online_Langs = {
 { id =  "ENGLISH_US",   version = "01.47",		author =  "Yoti" },
 { id =  "ITALIAN",		version = "01.52",		author =  "TheHeroGAC" },
 { id =  "JAPANESE",		version = "01.48",		author =  "Kouchan" },
-{ id =  "SPANISH",		version = "01.59",		author =  "tetsuoScene" },
+{ id =  "SPANISH",		version = "01.60",		author =  "gvaldebenit" },
 { id =  "CHINESE_S",	version = "01.67",		author =  "yexun1995" },
 { id =  "FRENCH",		version = "01.71",		author =  "Chronoss09" },
 { id =  "GERMAN",		version = "01.47",		author =  "Schn1ek3" },
