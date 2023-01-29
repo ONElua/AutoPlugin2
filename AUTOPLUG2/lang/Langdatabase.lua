961874830570497d154dc71864cc9caea2d3edd9
ONELua Team <oneluaproject@gmail.com>
2023-01-28 20:41:26 -0600

    RUSSIAN updated

--- AUTOPLUG2/lang/Langdatabase.lua
+++ AUTOPLUG2/lang/Langdatabase.lua
@@ -6,7 +6,7 @@ Langs = {
 { id =  "CHINESE_S",	version = "01.68",		author =  "yexun1995" },
 { id =  "FRENCH",		version = "01.72",		author =  "Chronoss09" },
 { id =  "GERMAN",		version = "01.48",		author =  "Schn1ek3" },
-{ id =  "RUSSIAN",		version = "01.70",		author =  "Yoti" },
+{ id =  "RUSSIAN",		version = "01.71",		author =  "Yoti" },
 { id =  "POLISH",		version = "01.49",		author =  "KierownikStacji" },
 { id =  "PORTUGUESE_BR",version = "01.35",		author =  "allanaln2009" },
 { id =  "KOREAN",       version = "01.03",		author =  "DDinghoya" },
