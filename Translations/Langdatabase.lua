a63ca1c5bb74aaab4cfa74231306c10d1088b3fc
ONELua Team <oneluaproject@gmail.com>
2023-07-16 17:52:51 -0600

    FRENCH & KOREAN

--- Translations/Langdatabase.lua
+++ Translations/Langdatabase.lua
@@ -4,10 +4,10 @@ Online_Langs = {
 { id =  "JAPANESE",		version = "01.55",		author =  "Kouchan" },
 { id =  "SPANISH",		version = "01.68",		author =  "gvaldebenit" },
 { id =  "CHINESE_S",	version = "01.75",		author =  "yexun1995" },
-{ id =  "FRENCH",		version = "01.82",		author =  "Axce" },
+{ id =  "FRENCH",		version = "01.83",		author =  "Axce" },
 { id =  "GERMAN",		version = "01.54",		author =  "Schn1ek3" },
 { id =  "RUSSIAN",		version = "01.79",		author =  "Yoti" },
 { id =  "POLISH",		version = "01.55",		author =  "KierownikStacji" },
 { id =  "PORTUGUESE_BR",version = "01.41",		author =  "allanaln2009" },
-{ id =  "KOREAN",       version = "01.15",		author =  "DDinghoya" },
+{ id =  "KOREAN",       version = "01.16",		author =  "DDinghoya" },
 }
