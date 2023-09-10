58b917b4f581aba02148c1afc0b3db67c574854d
ONELua Team <oneluaproject@gmail.com>
2023-09-09 21:17:21 -0600

    FRENCH&KOREAN Langs

--- AUTOPLUG2/lang/Langdatabase.lua
+++ AUTOPLUG2/lang/Langdatabase.lua
@@ -4,10 +4,10 @@ Langs = {
 { id =  "JAPANESE",		version = "01.56",		author =  "Kouchan" },
 { id =  "SPANISH",		version = "01.69",		author =  "gvaldebenit" },
 { id =  "CHINESE_S",	version = "01.77",		author =  "yexun1995" },
-{ id =  "FRENCH",		version = "01.84",		author =  "Axce" },
+{ id =  "FRENCH",		version = "01.85",		author =  "Axce,chronoss09" },
 { id =  "GERMAN",		version = "01.55",		author =  "Schn1ek3" },
 { id =  "RUSSIAN",		version = "01.80",		author =  "Yoti" },
 { id =  "POLISH",		version = "01.56",		author =  "KierownikStacji" },
 { id =  "PORTUGUESE_BR",version = "01.42",		author =  "allanaln2009" },
-{ id =  "KOREAN",       version = "01.17",		author =  "DDinghoya" },
+{ id =  "KOREAN",       version = "01.18",		author =  "DDinghoya" },
 }
