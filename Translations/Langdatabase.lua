2418bb59e7104c808bf526207a1b339e87801a03
ONELua Team <oneluaproject@gmail.com>
2023-11-14 17:43:30 -0600

    Updated CHINESE lang

--- Translations/Langdatabase.lua
+++ Translations/Langdatabase.lua
@@ -3,7 +3,7 @@ Online_Langs = {
 { id =  "ITALIAN",		version = "01.60",		author =  "TheHeroGAC" },
 { id =  "JAPANESE",		version = "01.56",		author =  "Kouchan" },
 { id =  "SPANISH",		version = "01.69",		author =  "gvaldebenit" },
-{ id =  "CHINESE_S",	version = "01.77",		author =  "yexun1995" },
+{ id =  "CHINESE_S",	version = "01.78",		author =  "yexun1995" },
 { id =  "FRENCH",		version = "01.85",		author =  "Axce,chronoss09" },
 { id =  "GERMAN",		version = "01.55",		author =  "Schn1ek3" },
 { id =  "RUSSIAN",		version = "01.80",		author =  "Yoti" },
