c02292f2c8e75d6cf3f26d69567913b38518c10f
ONELua Team <oneluaproject@gmail.com>
2024-01-13 20:00:09 -0600

    Add files via upload

--- AUTOPLUG2/lang/Langdatabase.lua
+++ AUTOPLUG2/lang/Langdatabase.lua
@@ -3,7 +3,7 @@ Langs = {
 { id =  "ITALIAN",		version = "01.61",		author =  "TheHeroGAC" },
 { id =  "JAPANESE",		version = "01.57",		author =  "Kouchan" },
 { id =  "SPANISH",		version = "01.70",		author =  "gvaldebenit" },
-{ id =  "CHINESE_S",	version = "01.78",		author =  "yexun1995" },
+{ id =  "CHINESE_S",	version = "01.79",		author =  "yexun1995" },
 { id =  "FRENCH",		version = "01.86",		author =  "Axce,chronoss09" },
 { id =  "GERMAN",		version = "01.56",		author =  "Schn1ek3" },
 { id =  "RUSSIAN",		version = "01.81",		author =  "Yoti" },
