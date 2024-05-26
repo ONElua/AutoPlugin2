c6564359553450eaecf978c330425bb07a4e7f80
ONELua Team <oneluaproject@gmail.com>
2024-05-25 20:26:56 -0600

    Add files via upload

--- Translations/Langdatabase.lua
+++ Translations/Langdatabase.lua
@@ -1,13 +1,13 @@
 Online_Langs = {
-{ id =  "ENGLISH_US",   version = "01.57",		author =  "Yoti" },
+{ id =  "ENGLISH_US",   version = "01.58",		author =  "Yoti" },
 { id =  "ITALIAN",		version = "01.62",		author =  "TheHeroGAC" },
 { id =  "JAPANESE",		version = "01.58",		author =  "Kouchan" },
 { id =  "SPANISH",		version = "01.72",		author =  "gvaldebenit" },
 { id =  "CHINESE_S",	version = "01.80",		author =  "yexun1995" },
-{ id =  "FRENCH",		version = "01.88",		author =  "Axce,chronoss09" },
+{ id =  "FRENCH",		version = "01.89",		author =  "Axce,chronoss09" },
 { id =  "GERMAN",		version = "01.57",		author =  "Schn1ek3" },
 { id =  "RUSSIAN",		version = "01.82",		author =  "Yoti" },
 { id =  "POLISH",		version = "01.58",		author =  "KierownikStacji" },
 { id =  "PORTUGUESE_BR",version = "01.44",		author =  "allanaln2009" },
-{ id =  "KOREAN",       version = "01.20",		author =  "DDinghoya" },
+{ id =  "KOREAN",       version = "01.21",		author =  "DDinghoya" },
 }
